13b6193fa5caa3cf5b8df6df88a048ba6914cfdd
sakshamgoyal06 <sakshamgoyal06@gmail.com>
2020-07-27 16:26:10 +0530

    navbar phase 1

---
+++ app/Assets/images/index.js
@@ -0,0 +1,3 @@
+export { default as ComingSoonGraphic } from './comingsoon.png';
+export { default as Businesses } from './businesses.png';
+export { default as Designers } from './designers.png';
---
+++ app/Assets/svg-comp/curve.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+function SvgCurve(props) {
+  return (
+    <svg viewBox="0 0 2032.147 492.854" {...props}>
+      <path
+        d="M0 0s63.087 470.339 649.417 330 577.029 175.8 996 162.441S2031.544 0 2031.544 0z"
+        fill="#f5f5f5"
+      />
+    </svg>
+  );
+}
+
+export default SvgCurve;
