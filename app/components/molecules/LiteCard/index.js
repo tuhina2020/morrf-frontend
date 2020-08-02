e4564402e8d9c97430835d531c5b80f86a166d44
sakshamgoyal06 <sakshamgoyal06@gmail.com>
2020-08-02 14:48:00 +0530

    lite phase 1

--- app/components/molecules/LiteCard/index.js
+++ app/components/molecules/LiteCard/index.js
@@ -12,7 +12,7 @@ style={{
       backgroundColor:"#F5F5F5",
         textAlign: 'center',
       paddingBottom: '24px',
-      }}
+      }}>
       <div style={{ backgroundColor:"#FFFFFF", width: "100%", height: "236px", marginBottom:"11px", }}></div>
       <div className="Ff($ffmanrope) Fw($fwregular)" style={{marginBottom: "16px", fontSize:"20px"}}>{title}</div>
       <div className="Ff($ffmanrope) Fw($fwregular)" style={{fontSize:"16px"}}>{description}</div>
