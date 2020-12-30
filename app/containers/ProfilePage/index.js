419c056914a3f3f01c7a07d82993bbe035f2ee20
charan12345678 <charanr52@gmail.com>
2020-12-30 10:57:58 +0530

    removed Tabs code

--- app/containers/ProfilePage/index.js
+++ app/containers/ProfilePage/index.js
@@ -1,6 +1,4 @@
 import React, { useEffect } from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
 
 import ProfileDetails from 'templates/ProfileDetails/desktop';
 
